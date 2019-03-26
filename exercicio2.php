3fe814fd20b0669805bbf7894743861a2014943a
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-03-26 11:08:33 -0300

    Começo e fim

---
+++ exercicio2.php
@@ -0,0 +1,13 @@
+<?php
+
+print "Digite o número: ";
+$numero= (float) fgets(STDIN);
+
+if ($numero<0)
+    {print "O número é negativo\n";}
+
+if ($numero>0)
+    {print "O número é positivo\n";}
+
+if ($numero==0)
+    {print "O número é neutro\n";}
