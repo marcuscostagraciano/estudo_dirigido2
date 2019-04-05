628ad92f5e7883013b9ddd454e90295f565aee69
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-04-05 18:45:17 -0300

    Caso os os 2 não funcionem graças à versão do PHP
    (obs: os outros dois funcionaram no meu PC, cuja versão do PHP é 7.3.2)

---
+++ exercicio3fgetc.php
@@ -0,0 +1,13 @@
+<?php
+
+print "Digite M ou F: ";
+$sexo = fgetc(STDIN);
+
+if ($sexo=="M" or $sexo=="m")
+    {print "Sexo Masculino";}
+
+elseif ($sexo=="F" or $sexo=="f")
+    {print "Sexo Feminino";}
+
+elseif ($sexo!="M" or $sexo!="m" or $sexo!="F" or $sexo!="f")
+    {print "Sexo inválido";}
