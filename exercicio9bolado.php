aeaa8bfef574461137adf06d0771074a765d2a9b
Marcus Vinícius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2019-04-13 17:37:49 -0300

    Exercício 9 full bolado, valeu sábado de tarde sem nada pra fazer

---
+++ exercicio9bolado.php
@@ -0,0 +1,34 @@
+<?php
+
+print "Número de números para comparação: ";
+$num= (float) fgets(STDIN);
+print "\n";
+
+$vezes=1;
+
+while ($vezes<=$num)
+    {print "Digite um número: ";
+    $numero= (float) fgets(STDIN);
+
+   if ($num==1){
+       $numeros= array("$numero");}
+    
+    elseif($num>1){
+        $numeros[$vezes]= $numero;
+    }
+$vezes++;}
+
+rsort($numeros);
+
+if (count($numeros)==1)
+    {print $numeros[0];}
+
+elseif (count($numeros)!=1)
+
+                $vezvariavel=0;
+                while ($i < count($numeros))
+                { print "\n".$numeros["$vezvariavel"];
+                  $vezvariavel++;
+                  $i++;}
+
+print "\n";
